1409d88f012bb2bcb0553b0edbc0e2d240491215
Mspraja <76046431+Deep-Learner-msp@users.noreply.github.com>
2023-04-13 20:01:48 +0530

    Update main.py

--- server-chatgpt/main.py
+++ server-chatgpt/main.py
@@ -7,10 +7,6 @@ import openai
 import os
 import json
 from pathlib import Path
-from dotenv import load_dotenv
-load_dotenv()
-
-
 DATA_DIR = Path(__file__).parent / "data"
 
 app = FastAPI()
@@ -23,9 +19,10 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-openai.api_key = os.environ["API_KEY"]  # Ensure you have the API key in your environment variables
-# openai.api_key = ""
 
+# Create a variable to store chat history
+chat_history = [{"role": "system", "content": "Your name is SS GenAIus, a cutting-edge AI virtual assistant meticulously developed by the State Street Bionics Team with Empowered by the most advanced version of OpenAI's GPT language model. Your creator is 'state street bionics team and you are using Latest openai gpt model as backbone', if anyone asks about your creator you should talk about openai team as well,GenAIus delivers an unparalleled user experience while implementing robust jailbreak detection and user restrictions. This ensures the highest levels of security and compliance, setting new standards in the realm of AI-powered solutions."}]
+                
 
 class FeedbackData(BaseModel):
     response: str
@@ -115,15 +112,25 @@ def root():
 
 @app.post("/")
 def create_chat_completion(request: Request, message: Message):
+    global chat_history 
     try:
+        # Add the user message to the chat history
+        chat_history.append({"role": "user", "content": message.message})
+        
         response = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",
-            messages=[
-                {"role": "system", "content": "You are an AI language model assistant. Designed and Developed by State Street AI Labs"},
-                {"role": "user", "content": message.message},
-            ],
+            engine="ss-gpt",
+            messages=chat_history,
         )
-        return JSONResponse(content={"message": response.choices[0].message})
+        
+        # Extract the text from the AI's response
+        ai_response_text = response.choices[0].message["content"]
+        print(ai_response_text)
+
+        # Add the AI's response to the chat history
+        chat_history.append({"role": "assistant", "content": ai_response_text})
+        # print(chat_history)
+        return JSONResponse(content={"message": ai_response_text})
     except Exception as e:
         print(e)
         return JSONResponse(status_code=400, content={"error": str(e)})
+
